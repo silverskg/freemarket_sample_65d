502e942794f2e154b7658564d83317e8e00bbc26
kudou_kyohei <kudou.pg1010@gmail.com>
2019-12-20 23:33:23 +0900

    環境によって画像のUP先を分岐

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.development? || Rails.env.test?
     storage :file
-  else
+  elsif.Rails.env.development?
     storage :fog
   end
 
