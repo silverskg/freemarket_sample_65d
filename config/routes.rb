0b55184995c048301a4d20fa2a91c17511540c4e
mizu0327 <yg0327955@gmail.com>
2019-12-24 12:49:04 +0900

    Login画面、新規登録画面のフロント実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: "items#index"
-  resources :items, only: [:index]
+  # root to: "items#index"
+  # resources :items, only: [:index]
+
+  root to: "login#index"
+  # root to: "registration#index_1"
+  # root to: "registration#index_2"
+  # root to: "registration#index_3"
+  # root to: "registration#index_4"
+  # root to: "registration#index_5"
+  # root to: "registration#index_6"
 end
