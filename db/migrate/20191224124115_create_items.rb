f588a47b6c77631c9b660a60ed6b3bddecb31f6a
silverskg <chokichoki_gm@i.softbank.jp>
2019-12-25 12:05:28 +0900

    未定テーブル作成

--- db/migrate/20191224124115_create_items.rb
+++ db/migrate/20191224124115_create_items.rb
@@ -10,8 +10,8 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string     :price,              null: false 
       t.text       :body,               null: false
       t.references     :user,            foreign_key: true
-      t.references     :brand,           foreign_key: true
-      t.references     :category,        foreign_key: true
+      # t.references     :brand,           foreign_key: true
+      # t.references     :category,        foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_013659) do
+ActiveRecord::Schema.define(version: 2019_12_25_015053) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2019_12_25_013659) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,5 +67,7 @@ ActiveRecord::Schema.define(version: 2019_12_25_013659) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "items", "users"
 end
