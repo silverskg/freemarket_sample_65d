140aadb956e16f702520d8a105809420e5698058
kudou_kyohei <kudou.pg1010@gmail.com>
2019-12-25 20:59:04 +0900

    不要なマイグレーションファイルを削除

--- db/migrate/20191225100812_drop_table_date.rb
+++
@@ -1,6 +0,0 @@
-class DropTableDate < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :years
-    drop_table :months
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_12_25_100812) do
-
+ActiveRecord::Schema.define(version: 2019_12_25_043048) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "build_name"
@@ -105,9 +103,11 @@ ActiveRecord::Schema.define(version: 2019_12_25_100812) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
   add_foreign_key "categories", "brands"
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
+  add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
