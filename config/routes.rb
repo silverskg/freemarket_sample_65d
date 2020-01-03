1457d7621fe2f02af0933fdec91757041d3ea7ea
sakimasaoka <58124066+sakimasaoka@users.noreply.github.com>
2020-01-03 14:31:07 +0900

    住所入力ヘッダー位置修正
    ヘッダーの位置を調整しました

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   # root to: "login#index"
 
   # ユーザー新規登録ページ表示
-  #root to: "registration#index_0"
-  #  root to: "registration#index_1"
-  #  root to: "registration#index_2"
-  #  root to: "registration#index_3"
+  # root to: "registration#index_0"
+  # root to: "registration#index_1"
+  # root to: "registration#index_2"
+  # root to: "registration#index_3"
   # root to: "registration#index_4"
   # root to: "registration#index_5"
   # root to: "registration#index_6"
