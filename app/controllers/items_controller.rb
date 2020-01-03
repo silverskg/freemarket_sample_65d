446b6fc4f2cf1917efa9055cbf25b380278aeeca
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-03 19:25:19 +0900

    itemをストロングパラメーターで作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,25 @@ class ItemsController < ApplicationController
   end
 
   def create
+    item = Item.create(item_params)
+    
+  end
+
+  private
+  # ユーザーidは、ユーザー登録後に実装(現在は仮で1を挿入)
+  def item_params
+    params.require(:item).permit(
+      :name, 
+      :status, 
+      :body, 
+      :deliver_fee, 
+      :delivery_data, 
+      :how_to_deliver, 
+      :region, 
+      :price, 
+      :category_id, 
+      :brand_id
+    ).merge(user_id: 1)
   end
   
 end
