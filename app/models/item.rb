3433ec8b0f58e18723c89b4c3db9d91f4873ed3f
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-04 23:36:46 +0900

    itemとimageテーブルにフォームのネスト関係を記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
     belongs_to :category
     belongs_to :brand
-    has_many :images
+    has_many :images, dependent: destroy
+    accepts_nested_attributes_for :images
     has_many :comments
     has_many :likes
 end
