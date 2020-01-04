f22c4c6764a5595b7a1b4e47b3ccfc502e4ba575
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-05 01:12:23 +0900

    複数枚対応かもしれないコードをコメントアウトで記載

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :item, optional: true
+
+  # mount_uploaders :image, ImageUploader
+  # belongs_to :item, optional: true, inverse_of: :images
 end
