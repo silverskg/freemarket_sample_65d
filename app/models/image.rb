d994129c06d20c1b268142a18cd14680cf0953a3
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-05 04:09:05 +0900

    オプションがなくても動く為、削除

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
   mount_uploader :image, ImageUploader
-  belongs_to :item, optional: true
+  belongs_to :item
 
   # mount_uploaders :image, ImageUploader
   # belongs_to :item, optional: true, inverse_of: :images
