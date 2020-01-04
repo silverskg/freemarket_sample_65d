6c00d3b277470b54cab31a361f0d447104dcf294
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-04 23:57:46 +0900

    アソシエーションのコロンつけ忘れ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
     belongs_to :category
     belongs_to :brand
-    has_many :images, dependent: destroy
+    has_many :images, dependent: :destroy
     accepts_nested_attributes_for :images
     has_many :comments
     has_many :likes
