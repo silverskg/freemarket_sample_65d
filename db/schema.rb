c1d05b2f1f20003ce230672cdc68a4fc780164ba
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-06 03:25:50 +0900

    itemテーブルにimageが含まれているかを判断するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_075204) do
+ActiveRecord::Schema.define(version: 2020_01_05_181857) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "build_name"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2020_01_03_075204) do
     t.datetime "updated_at", null: false
     t.bigint "brand_id"
     t.bigint "category_id"
+    t.string "include_image"
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
