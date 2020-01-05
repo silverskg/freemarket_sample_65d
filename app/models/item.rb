1b98155a5f04eab48cb850e4f95e51ecf2af182a
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-06 04:05:54 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,14 @@ class Item < ApplicationRecord
     accepts_nested_attributes_for :images
     has_many :comments
     has_many :likes
+
+    validates :name, presence: true, length:{ maximum: 40 }
+    validates :status, presence: true
+    validates :body, presence: true, length:{ maximum: 999 }
+    validates :deliver_fee, presence: true
+    validates :delivery_date, presence: true
+    validates :how_to_deliver, presence: true
+    validates :region, presence: true
+    validates :price, presence: true, numericality: {only_integer: true, greater_than: 300, less_than: 10000000 }
+    validates :include_image, presence: true
 end
