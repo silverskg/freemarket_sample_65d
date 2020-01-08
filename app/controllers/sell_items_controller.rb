c0706e2101592f79d07cad9645709ae324cb16f9
mizu0327 <yg0327955@gmail.com>
2020-01-08 14:52:57 +0900

    prefixなど修正

--- app/controllers/sell_items_controller.rb
+++ app/controllers/sell_items_controller.rb
@@ -1,18 +1,27 @@
 class SellItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
+
   def index
     @items = Item.all.includes(:images)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to action: 'index'
+    if @item.destroy
+      redirect_to action: 'index'
+    else
+      redirect_to action: 'index'
+    end
   end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
