c4c3143735b4b14c9a2dff949a9f3f7a34d65c03
moromotomasahiro <moromotomasahiro@icloud.com>
2020-01-08 18:43:16 +0900

    コメント削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all.order(id: "DESC").includes(:images)
-    # @items = Item.where(category_id: 2).includes(:images)
   end
 
   def new
