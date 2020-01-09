475cf4d59640985157b8d5114406336a576a147f
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-09 19:56:17 +0900

    file_fieldのmultipull属性を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,26 +7,19 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new 
-    #ネストしたテーブルを作成
-    @item.images.build
   end
 
   def create
     @item = Item.new(item_params)
-    #imageがアップされている場合
-    if params[:images]
+
+    #画像ファイルがUPされている場合
+    if params[:item][:images_attributes]
       @item.include_image = "include"
     end
 
     if @item.save
-      #file_fieldのparams(name属性)に含まれる複数のimageを分解
-      params[:images][:image].each do |image|
-        #アソシエーションを使い、itemテーブルを通してimageテーブルに作成
-        @item.images.create(image: image, item_id: @item.id)
-      end
       redirect_to root_path
     else
-      @item.images.build
       render :new
     end
   end
@@ -72,8 +65,8 @@ class ItemsController < ApplicationController
       :price, 
       :category_id, 
       :brand_id,
-      #field_forで設定した値+_attributesで受け取る。複数の為{file_field属性名: []}で受け取る。
-      images_attributes: {image: []}
+      #field_forで設定した値+_attributesで受け取る。
+      images_attributes: [:image]
     ).merge(user_id: 1)
   end
 
