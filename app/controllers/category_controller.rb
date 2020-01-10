16a0e72a0ad358673ac9ca784b4d9f77fc8ce95c
mizu0327 <yg0327955@gmail.com>
2020-01-10 20:18:47 +0900

    カテゴリーに属する商品一覧ページ実装

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,7 +3,8 @@ class CategoryController < ApplicationController
     @categories = Category.all
   end
 
-  def search
+  def show
     @category = Category.find(params[:id])
+    @items = @category.items.order(created_at: :desc).all.page(params[:page]).per(5)
   end
 end
