5309d81aec356f9b47c87701a819e78bd90c6c78
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-13 12:33:33 +0900

    出品中の商品詳細画面の各内容をDBの内容に差し替え

--- app/controllers/sell_items_controller.rb
+++ app/controllers/sell_items_controller.rb
@@ -18,5 +18,12 @@ class SellItemsController < ApplicationController
   private
   def set_item
     @item = Item.find(params[:id])
+    @user = User.find(@item.user_id)
+    @category = Category.find(@item.category_id)
+    @brand = Brand.find(@item.brand_id)
+    @prefecture = Prefecture.find(@item.region)
+    @brand_items = Item.where(brand_id: @item.brand_id)
+    @user_items = Item.where(user_id: @item.user_id)
+    @images = Image.where(item_id:  @item.id)
   end
 end
