918b863a3aa325b505a18425b317dce0c16248bc
moromotomasahiro <moromotomasahiro@icloud.com>
2020-01-14 14:12:27 +0900

    pay.jp編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  
   # ログイン画面表示
   devise_for :users, controllers: {
     registrations:  'users/registrations'
