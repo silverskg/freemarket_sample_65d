001bb3cdbc684a4ce60781d6aec843d5d36fe3fc
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-14 19:16:32 +0900

    アイテム購入時のアクションと内容を設定

--- app/controllers/product_confirmation_controller.rb
+++ app/controllers/product_confirmation_controller.rb
@@ -1,9 +1,17 @@
 class ProductConfirmationController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: :index
+  before_action :set_item, only: [:index, :sell_item]
   before_action :set_user, only: :index
+  before_action :check_user, only: :sell_item
+  before_action :check_item, only: :sell_item
 
   def index
+  end
+
+  def sell_item
+    @item.sale_status = "sold_out"
+    @item.save
+    redirect_to root_path
   end
 
   def set_item
@@ -15,5 +23,17 @@ class ProductConfirmationController < ApplicationController
     @user = User.find(current_user.id)
     @address = Address.find_by(user_id: @user.id)
     @prefecture = Prefecture.find(@address.prefectures)
+  end
+
+  def check_user
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def check_item
+    if @item.sale_status == "sold_out"
+      redirect_to root_path
+    end
   end
 end
