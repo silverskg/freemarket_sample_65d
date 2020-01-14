0aa7926873828344530a9a1744e8f87e356b4770
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-14 20:53:39 +0900

    クレジットカード登録情報を表示し、カード未登録の場合は購入できないように設定

--- app/controllers/product_confirmation_controller.rb
+++ app/controllers/product_confirmation_controller.rb
@@ -2,6 +2,7 @@ class ProductConfirmationController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: :index
   before_action :set_user, only: :index
+  before_action :set_card, only: :index
 
   def index
   end
@@ -15,6 +16,17 @@ class ProductConfirmationController < ApplicationController
     @user = User.find(current_user.id)
     @address = Address.find_by(user_id: @user.id)
     @prefecture = Prefecture.find(@address.prefectures)
+  end
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
+    if @card.blank?
+      # redirect_to action: "new" 
+    else
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
   end
   
 end
