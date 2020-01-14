d3104c4b8cc5f90eb31897d9f62fb31f436dd0e7
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-14 20:29:11 +0900

    購入処理を行うコントローラーを変更

--- app/controllers/product_confirmation_controller.rb
+++ app/controllers/product_confirmation_controller.rb
@@ -1,17 +1,9 @@
 class ProductConfirmationController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_item, only: [:index, :sell_item]
+  before_action :set_item, only: :index
   before_action :set_user, only: :index
-  before_action :check_user, only: :sell_item
-  before_action :check_item, only: :sell_item
 
   def index
-  end
-
-  def sell_item
-    @item.sale_status = "sold_out"
-    @item.save
-    redirect_to root_path
   end
 
   def set_item
@@ -24,16 +16,5 @@ class ProductConfirmationController < ApplicationController
     @address = Address.find_by(user_id: @user.id)
     @prefecture = Prefecture.find(@address.prefectures)
   end
-
-  def check_user
-    if @item.user_id == current_user.id
-      redirect_to root_path
-    end
-  end
-
-  def check_item
-    if @item.sale_status == "sold_out"
-      redirect_to root_path
-    end
-  end
+  
 end
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,6 +1,8 @@
 class PurchaseController < ApplicationController
 
   require 'payjp'
+  before_action :check_user, only: :sell_item
+  before_action :check_item, only: :sell_item
 
   def index
     card = Card.where(user_id: current_user.id).first
@@ -9,7 +11,7 @@ class PurchaseController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -18,14 +20,31 @@ class PurchaseController < ApplicationController
   end
 
   def pay
+    @item = Item.find(params[:item_id])
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+    binding.pry
     Payjp::Charge.create(
-    :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
+    :amount => @item.price, #支払金額を入力（送料は一旦無視)
     :customer => card.customer_id, #顧客ID
     :currency => 'jpy', #日本円
   )
-  redirect_to action: 'done' #完了画面に移動
+    @item.sale_status = "sold_out"
+    @item.save
+    redirect_to root_path
+  # redirect_to action: 'done' #完了画面に移動
+  end
+
+  def check_user
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def check_item
+    if @item.sale_status == "sold_out"
+      redirect_to root_path
+    end
   end
 
 end
