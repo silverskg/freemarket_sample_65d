a021bf7ca73f56234695c3c95eaeb834985ab5f7
moromotomasahiro <moromotomasahiro@icloud.com>
2020-01-14 15:01:27 +0900

    編集２

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,8 @@ Rails.application.routes.draw do
     post 'step3_registrations', to: 'users/registrations#step3'
     post 'step4_registrations', to: 'users/registrations#step4'
     post 'step5_address_form_registrations', to: 'users/registrations#step5_address_form'
-    post 'step6_payjp_registrations', to: 'users/registrations#step6_payjp'
-    get 'step7_registrations', to: 'users/registrations#step7'
+    get 'step6_payjp_registrations', to: 'users/registrations#step6_payjp'
+    post 'step7_registrations', to: 'users/registrations#step7'
     get  'user_registration', to: 'users/registrations#create'
   end
 
