08a63e1e0dd709da3f5b9e82fba9fbb027e9547b
kudou_kyohei <kudou.pg1010@gmail.com>
2020-01-14 17:52:48 +0900

    必要な情報を取得

--- app/controllers/product_confirmation_controller.rb
+++ app/controllers/product_confirmation_controller.rb
@@ -1,11 +1,19 @@
 class ProductConfirmationController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: :index
+  before_action :set_user, only: :index
 
   def index
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+    @image = Image.find_by(item_id: @item.id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
+    @address = Address.find_by(user_id: @user.id)
+    @prefecture = Prefecture.find(@address.prefectures)
   end
 end
