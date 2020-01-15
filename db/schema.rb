f9eb005cdb78b82c1f5829bacb5e324cf4809da3
sakimasaoka <58124066+sakimasaoka@users.noreply.github.com>
2020-01-15 09:40:11 +0900

    ビュー崩れ訂正
    ビュー崩れ訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_123059) do
+
+ActiveRecord::Schema.define(version: 2020_01_14_073329) do
+
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "build_name"
